0bf5631220735242d322c6247a71769287611a55
Karthik Kovi <Karthi.kovi@gmail.com>
2020-03-26 16:45:26 +1030

    Created basic variables and emptied the planner div if any elements are present

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
 
+    // Array to store plans to build the calender
+    let planArray;
+
+    //Getting current date using Moment and updating it in HTML
     const currentDate = moment().format('MMMM Do YYYY');
     $('#currentDate').text(currentDate);
+
+    //Reading any existing stored plans
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+    //building array for planner
+
+    if (storedPlans === null){
+        planArray = new Array(9);
+    } else {
+        planArray = storedPlans;
+    }
+
+    // reading the Planner div from HTML
+
+    const $plannerDiv = $("#plannerDiv");
+
+    // emptying the planner Div if any text is present
+
+    $plannerDiv.empty();
 
 })
